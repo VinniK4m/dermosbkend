11283ac4b103931f391c66e43f086dcc27b939c2
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-02-04 12:33:53 -0500

    commit hu01: Actualiza Cors para Medico

--- dermosbkend/settings.py
+++ dermosbkend/settings.py
@@ -45,13 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dermobkend.apps.DermobkendConfig',
     'dermosbkend',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,7 +61,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 ]
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+    'http://localhost:8000'
 
+)
 ROOT_URLCONF = 'dermosbkend.urls'
 
 TEMPLATES = [
