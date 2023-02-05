b26b7aca2f54d6c0b485d05213e5aa74b2638562
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-02-05 17:57:03 -0500

    Update settings.py
    Adiciona URL 'https://dermosbkend.onrender.com' en el cors_filter

--- config/settings.py
+++ config/settings.py
@@ -71,11 +71,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
     'http://127.0.0.1:4200',
     'http://127.0.0.1:8000',
-)
-
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:4200',
-    'http://localhost:8000'
+    'https://dermosbkend.onrender.com'
 )
 
 ROOT_URLCONF = 'config.urls'
