f4566d296a07ad6bd2ca4b3b4175992e4f8a29f1
Juan Acevedo <j.acevedob@uniandes.edu.co>
2023-02-22 23:58:10 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -74,7 +74,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:4200',
     'http://127.0.0.1:8000',
     'https://dermosbkend.onrender.com',
-    'https://dermoweb.onrender.com'
+    'https://dermoweb.onrender.com',
+    'https://dermobuild.onrender.com'/
 )
 
 ROOT_URLCONF = 'config.urls'
