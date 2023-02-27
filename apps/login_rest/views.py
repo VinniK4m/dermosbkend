e3fb36cb6be8c27c3ff15f273ed77489f5ea4405
cesarchembi <cesar44@gmail.com>
2023-02-26 23:23:54 -0500

    HU21 Autenticación Token librerias

--- apps/login_rest/views.py
+++ apps/login_rest/views.py
@@ -16,8 +16,6 @@ from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import AccessToken
 
 
 class UserLogIn(ObtainAuthToken):
